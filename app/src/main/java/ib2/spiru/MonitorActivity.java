5aff3da02824342d9ed6439d3fb8cb0e1ff36764
Leticia Marques <leticia.marquespinho@gmail.com>
2022-08-03 10:42:28 -0300

    Added MonitorActivity to screens

---
+++ app/src/main/java/ib2/spiru/MonitorActivity.java
@@ -0,0 +1,14 @@
+package ib2.spiru;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MonitorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monitor);
+    }
+}
