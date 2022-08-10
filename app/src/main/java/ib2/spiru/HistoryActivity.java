21177db7076a371e7374314f00e62801c7c9e40f
Leticia Marques <leeh.marques157@gmail.com>
2022-08-10 08:47:37 -0300

    Add code to History and Monitor buttons

--- app/src/main/java/ib2/spiru/HistoryActivity.java
+++ app/src/main/java/ib2/spiru/HistoryActivity.java
@@ -2,7 +2,9 @@ package ib2.spiru;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HistoryActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class HistoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+        androidx.appcompat.widget.AppCompatButton historyBackButton =  findViewById(R.id.history_back);
+        historyBackButton.setOnClickListener((View.OnClickListener) this);
+    }
+
+    public void onClick(View v) {
+        Intent intent = new Intent(HistoryActivity.this, MonitorActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/ib2/spiru/MonitorActivity.java
+++ app/src/main/java/ib2/spiru/MonitorActivity.java
@@ -2,7 +2,10 @@ package ib2.spiru;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MonitorActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MonitorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monitor);
+        Button goToHistoryButton =  findViewById(R.id.go_to_history_button);
+        goToHistoryButton.setOnClickListener((View.OnClickListener) this);
+    }
+
+    public void onClick(View v) {
+        Intent intent = new Intent(MonitorActivity.this, HistoryActivity.class);
+        startActivity(intent);
     }
 }
