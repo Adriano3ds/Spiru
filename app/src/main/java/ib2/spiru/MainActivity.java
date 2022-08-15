0a56f9aabbd4ad8c6f8f95dfdbe045487d5af412
Leticia Marques <leeh.marques157@gmail.com>
2022-08-15 16:09:20 -0300

    Create buttons main screen

--- app/src/main/java/ib2/spiru/MainActivity.java
+++ app/src/main/java/ib2/spiru/MainActivity.java
@@ -15,11 +15,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ImageButton beginButton = (ImageButton)findViewById(R.id.btnStart);
+        Button historyButton = (Button)findViewById(R.id.go_to_history_button_main);
+        Button settingsButton = (Button)findViewById(R.id.settings_button_main);
 
         beginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, MonitorActivity.class));
+            }
+        });
+        historyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, HistoryActivity.class));
+            }
+        });
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, BluetoothActivity.class));
             }
         });
     }
