cb04d16502cea447b3b0da211aeafb17910f1cb4
Adriano Junior <jr.adrianogm@gmail.com>
2022-08-15 17:07:44 -0300

    Reset BluetoothActivity.java

--- app/src/main/java/ib2/spiru/BluetoothActivity.java
+++ app/src/main/java/ib2/spiru/BluetoothActivity.java
@@ -33,39 +33,6 @@ public class BluetoothActivity extends AppCompatActivity {
 
     com.google.android.material.floatingactionbutton.FloatingActionButton scanButton;
     public static ListView scanListView;
-    ArrayList<String> stringArrayList = new ArrayList<String>();
-    ArrayAdapter<String> arrayAdapter;
-    final int REQUEST_ENABLE_BT = 1;
-    final int REQUEST_BLUETOOTH_PERMISSIONS = 2;
-
-    private BluetoothAdapter bluetoothAdapter;
-    private boolean mScanning;
-    private Handler handler;
-
-    // Stops scanning after 10 seconds.
-    private static final long SCAN_PERIOD = 10000;
-
-    private LeDeviceListAdapter leDeviceListAdapter;
-    // Device scan callback.
-    private BluetoothAdapter.LeScanCallback leScanCallback =
-            new BluetoothAdapter.LeScanCallback() {
-                @Override
-                public void onLeScan(final BluetoothDevice device, int rssi,
-                                     byte[] scanRecord) {
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            if (ActivityCompat.checkSelfPermission(BluetoothActivity.this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                                return;
-                            }
-                            arrayAdapter.add(device.getName());
-                            arrayAdapter.notifyDataSetChanged();
-//                            leDeviceListAdapter.addDevice(device);
-//                            leDeviceListAdapter.notifyDataSetChanged();
-                        }
-                    });
-                }
-            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,119 +41,16 @@ public class BluetoothActivity extends AppCompatActivity {
         scanButton = findViewById(R.id.scanButton);
         scanListView = findViewById(R.id.scanLV);
 
-        final BluetoothManager bluetoothManager =
-                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-        bluetoothAdapter = bluetoothManager.getAdapter();
-
-        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH_CONNECT,
-                        Manifest.permission.BLUETOOTH_SCAN, Manifest.permission.ACCESS_COARSE_LOCATION,
-                        Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_BLUETOOTH_PERMISSIONS);
-                return;
-            }
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
-
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        registerReceiver(receiver, filter);
-        arrayAdapter = new ArrayAdapter<String>(getApplicationContext(),
-                android.R.layout.simple_list_item_1, stringArrayList);
-        scanListView.setAdapter(arrayAdapter);
-
         scanButton.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("MissingPermission")
             @Override
             public void onClick(View v) {
-                scanLeDevice(true);
-//                int requestCode = 1;
-//                Intent discoverableIntent =
-//                        new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-//                discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
-//                startActivityForResult(discoverableIntent, requestCode);
-//                if (bluetoothAdapter == null) {
-//                    Toast.makeText(getApplicationContext(), "Bluetooth Not Supported", Toast.LENGTH_SHORT).show();
-//                } else {
-//                    Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
-//                    if (pairedDevices.size() > 0) {
-//                        for (BluetoothDevice device : pairedDevices) {
-//                            String devicename = device.getName();
-//                            stringArrayList.add(devicename);
-//                            Log.d("Bluetooth ", devicename);
-//                            arrayAdapter.notifyDataSetChanged();
-//                        }
-//                    }
-//                }
+
             }
         });
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case REQUEST_BLUETOOTH_PERMISSIONS: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // permission was granted, yay!
-                    Intent enableIntent = new Intent(
-                            BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                    //startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
-                } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                    Toast.makeText(this, "Permission denied for bluetooth", Toast.LENGTH_SHORT).show();
-                }
-                return;
-            }
-        }
-    }
-
-    private void scanLeDevice(final boolean enable) {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        if (enable) {
-            // Stops scanning after a pre-defined scan period.
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    mScanning = false;
-                    if (ActivityCompat.checkSelfPermission(BluetoothActivity.this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
-                        return;
-                    }
-                    bluetoothAdapter.stopLeScan(leScanCallback);
-                }
-            }, SCAN_PERIOD);
-
-            mScanning = true;
-            bluetoothAdapter.startLeScan(leScanCallback);
-        } else {
-            mScanning = false;
-            bluetoothAdapter.stopLeScan(leScanCallback);
-        }
-    }
-
-    private final BroadcastReceiver receiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                // Discovery has found a device. Get the BluetoothDevice
-                // object and its info from the Intent.
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                if (ActivityCompat.checkSelfPermission(BluetoothActivity.this,
-                        Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                    return;
-                }
-                String deviceName = device.getName();
-                String deviceHardwareAddress = device.getAddress(); // MAC address
-            }
-        }
-    };
-    @Override
     protected void onDestroy() {
         super.onDestroy();
-        unregisterReceiver(receiver);
     }
 }
